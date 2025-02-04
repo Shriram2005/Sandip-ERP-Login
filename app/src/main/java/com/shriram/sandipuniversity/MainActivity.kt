ab4b7c2965922d4af334669073f5c7299945ffe0
Shriram Mange <shrirammange12345@gmail.com>
2025-02-04 13:31:16 +0530

    updated to latest version

--- app/src/main/java/com/shriram/sandipuniversity/MainActivity.kt
+++ app/src/main/java/com/shriram/sandipuniversity/MainActivity.kt
@@ -7,11 +7,13 @@ import android.net.NetworkCapabilities
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
